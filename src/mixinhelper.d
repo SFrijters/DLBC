24471dce2d00629688b4d18b19cc61aff0fb3a73
Stefan Frijters <sfrijters@gmail.com>
2014-04-04 16:08:07 +0200

    Clean up mixinhelper.d.

--- src/mixinhelper.d
+++ src/mixinhelper.d
@@ -1,15 +1,18 @@
 /* http://www.d-programming-language.org/templates-revisited.html
    " Template Metaprogramming With Strings " */
-template decimalDigit(int n) { 
+template decimalDigit(const int n) {
   const string decimalDigit = "0123456789"[n..n+1];
-} 
+}
 
-template itoa(long n) { 
-  static if (n < 0) 
+template itoa(const long n) {
+  static if (n < 0) {
     const string itoa = "-" ~ itoa!(-n);
-  else static if (n < 10)
+  }
+  else static if (n < 10) {
     const string itoa = decimalDigit!(n);
-  else
+  }
+  else {
     const string itoa = itoa!(n/10L) ~ decimalDigit!(n%10L);
-} 
+  }
+}
 
