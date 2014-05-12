7928f1e81f3ba27adfe17d261421f9e9fe89961e
Stefan Frijters <sfrijters@gmail.com>
2014-05-12 15:20:41 +0200

    Add more documentation.

--- src/dlbc/mixinhelper.d
+++ src/dlbc/mixinhelper.d
@@ -1,11 +1,41 @@
+// Written in the D programming language.
+
+/**
+   Helper functions and templates for CTFE. Based on 
+   $(LINK http://www.d-programming-language.org/templates-revisited.html)
+   " Template Metaprogramming With Strings "
+
+Copyright: Stefan Frijters 2011-2014
+
+License: $(HTTP boost.org/LICENSE_1_0.txt, Boost License 1.0).
+
+Authors: Stefan Frijters
+
+Macros:
+	TR = <tr>$0</tr>
+	TH = <th>$0</th>
+	TD = <td>$0</td>
+	TABLE = <table border=1 cellpadding=4 cellspacing=0>$0</table>
+*/
+
 module dlbc.mixinhelper;
 
-/* http://www.d-programming-language.org/templates-revisited.html
-   " Template Metaprogramming With Strings " */
+/**
+   Converts a digit to a string at compile time.
+
+   Param:
+     n = integer digit
+*/
 template decimalDigit(const int n) {
   const string decimalDigit = "0123456789"[n..n+1];
 }
 
+/**
+   Converts an integer to a string at compile time.
+
+   Param:
+     n = integer to convert
+*/
 template itoa(const long n) {
   static if (n < 0) {
     const string itoa = "-" ~ itoa!(-n);
