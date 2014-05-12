1c84e5f22d57027bfe6c810686c1d6f6814988ee
Stefan Frijters <sfrijters@gmail.com>
2014-05-12 13:51:05 +0200

    Add documentation.

--- src/dlbc/getopt.d
+++ src/dlbc/getopt.d
@@ -1,3 +1,24 @@
+// Written in the D programming language.
+
+/**
+Process command line arguments.
+
+This module uses $(D std.getopt) as a parser, and processes the result
+according to the need of this program.
+
+Copyright: Stefan Frijters 2011-2014
+
+License: $(HTTP boost.org/LICENSE_1_0.txt, Boost License 1.0).
+
+Authors: Stefan Frijters
+
+Macros:
+	TR = <tr>$0</tr>
+	TH = <th>$0</th>
+	TD = <td>$0</td>
+	TABLE = <table border=1 cellpadding=4 cellspacing=0>$0</table>
+*/
+
 module dlbc.getopt;
 
 import std.getopt;
@@ -5,15 +26,55 @@ import std.getopt;
 import dlbc.logging;
 import dlbc.parameters: parameterFileNames;
 
-/// Process CLI
+/**
+   Usage help text.
+*/
+private auto usage = `
+Parallel implementation of the lattice Boltzmann method for the simulation
+of fluid dynamics.
+
+Usage:
+%s [options]
+
+Options (defaults in brackets):
+  -h                   show this help message and exit
+  -p <path>            path to parameter file (can be specified multiple times)
+  -v <level>           set verbosity level to one of (Off, Fatal, Error,
+                       Warning, Notification, Information, Debug) [%s]
+  -w                   warnings and above are fatal
+`;
+
+/**
+   Process the command line arguments using $(D std.getopt).
+
+   Params:
+     args = command line arguments
+*/
 void processCLI(string[] args) {
+  bool showHelp = false;
+  auto fileName = args[0];
+  if ( args.length <= 1 ) {
+    showHelp = true;
+  }
+  
   writeLogRN("Processing command line arguments.");
+
   VL verbosityLevel = getGlobalVerbosityLevel();
+
   getopt( args,
+          "h", &showHelp,
           "p|parameterfile", &parameterFileNames,
           "v|verbose", &verbosityLevel,
           "W", &warningsAreFatal,
           );
+
+  if ( showHelp ) {
+    import std.c.stdlib: exit;
+    writeLogRN(makeHeaderString("Usage help:"));
+    writeLogRN(usage, args[0], verbosityLevel);
+    exit(0);
+  }
+
   setGlobalVerbosityLevel(verbosityLevel);
 }
 
--- src/dlbc/random.d
+++ src/dlbc/random.d
@@ -1,3 +1,21 @@
+// Written in the D programming language.
+
+/**
+   Helper functions for random number generation.
+
+   Copyright: Stefan Frijters 2011-2014
+
+   License: $(HTTP boost.org/LICENSE_1_0.txt, Boost License 1.0).
+
+   Authors: Stefan Frijters
+
+   Macros:
+        TR = <tr>$0</tr>
+        TH = <th>$0</th>
+	TD = <td>$0</td>
+	TABLE = <table border=1 cellpadding=4 cellspacing=0>$0</table>
+*/
+
 module dlbc.random;
 
 import dlbc.logging;
@@ -5,12 +23,24 @@ import dlbc.parallel;
 
 import std.random;
 
+/**
+  The seed for the PRNG to be used. Depending on the initialisation this value
+  may be modified according to the rank of the process to avoid the creation
+  of identical domains.
+*/
 @("param") int seed;
 
+/**
+  The PRNG is currently the Mersenne Twister with standard initialisation values.
+*/
 Mt19937 rng;
 
+/**
+   Initialises the PRNG on each process.
+*/
 void initRNG() {
   writeLogRI("Initializing random number generator of type '%s' with seed %d plus MPI rank.", typeof(rng).stringof, seed);
+  // Each process gets a different seed.
   rng.seed(seed + M.rank);
 }
 
