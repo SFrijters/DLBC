82b8cff415ede1078dd804c6324cda916f157b5b
Stefan Frijters <sfrijters@gmail.com>
2014-05-16 16:56:54 +0200

    Add group functions and constants to mpi header.

--- src/dlbc/mpi.d
+++ src/dlbc/mpi.d
@@ -118,4 +118,24 @@ immutable MPI_Op MPI_MAXLOC  = cast(MPI_Op)(0x5800000c);
 immutable MPI_Op MPI_REPLACE = cast(MPI_Op)(0x5800000d);
 immutable MPI_Op MPI_NO_OP   = cast(MPI_Op)(0x5800000e);
 
+alias int MPI_Group;
+immutable MPI_Group MPI_GROUP_EMPTY = cast(MPI_Group) 0x48000000;
 
+immutable MPI_Group MPI_GROUP_NULL = cast(MPI_Group) 0x08000000;
+immutable MPI_Comm MPI_COMM_NULL = cast(MPI_Comm) 0x04000000;
+
+int MPI_Group_size(MPI_Group group, int *size);
+int MPI_Group_rank(MPI_Group group, int *rank);
+int MPI_Group_translate_ranks(MPI_Group group1, int n, const int ranks1[], MPI_Group group2, int ranks2[]);
+int MPI_Group_compare(MPI_Group group1, MPI_Group group2, int *result);
+int MPI_Comm_group(MPI_Comm comm, MPI_Group *group);
+int MPI_Group_union(MPI_Group group1, MPI_Group group2, MPI_Group *newgroup);
+int MPI_Group_intersection(MPI_Group group1, MPI_Group group2, MPI_Group *newgroup);
+int MPI_Group_difference(MPI_Group group1, MPI_Group group2, MPI_Group *newgroup);
+int MPI_Group_incl(MPI_Group group, int n, const int ranks[], MPI_Group *newgroup);
+int MPI_Group_excl(MPI_Group group, int n, const int ranks[], MPI_Group *newgroup);
+int MPI_Group_range_incl(MPI_Group group, int n, int ranges[][3], MPI_Group *newgroup);
+int MPI_Group_range_excl(MPI_Group group, int n, int ranges[][3], MPI_Group *newgroup);
+int MPI_Group_free(MPI_Group *group);
+
+int MPI_Comm_create(MPI_Comm comm, MPI_Group group, MPI_Comm *newcomm);
