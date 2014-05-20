bea39c016cd628a54b0f1e98da77a9cfefe6356e
Stefan Frijters <sfrijters@gmail.com>
2014-05-20 21:30:29 +0200

    Fix license links in headers, again.

--- src/dlbc/parameters.d
+++ src/dlbc/parameters.d
@@ -56,7 +56,7 @@
 
    Copyright: Stefan Frijters 2011-2014
 
-   License: $(HTTP www.gnu.org/licenses/gpl-3.0.txt, GNU General Public License, version 3 (GPL-3.0)).
+   License: $(HTTP www.gnu.org/licenses/gpl-3.0.txt, GNU General Public License - version 3 (GPL-3.0)).
 
    Authors: Stefan Frijters
 
