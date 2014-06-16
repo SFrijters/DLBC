a331e974d2e99fb436f32a78c4ab5bbd48f4d5b9
Stefan Frijters <sfrijters@gmail.com>
2014-06-16 13:58:52 +0200

    Fix initEqDistSphere and add initEqDistSphereFrac.

--- src/dlbc/fields/init.d
+++ src/dlbc/fields/init.d
@@ -86,7 +86,37 @@ void initEqDistSphere(alias conn, T)(ref T field, const double density1, const d
     double[conn.d] gn, offset;
     foreach(immutable i; Iota!(0, conn.d) ) {
       gn[i] = p[i] + M.c[i] * field.n[i] - to!double(field.haloSize);
-      offset[i] = gn[i] - to!double(M.nc[i] * field.n[i] / 2);
+      offset[i] = gn[i] - to!double(M.nc[i] * field.n[i] / 2) + 0.5;
+    }
+    if ( offset.dotProduct(offset) < r2 ) {
+      e = pop1;
+    }
+    else {
+      e = pop2;
+    }
+  }
+}
+
+void initEqDistSphereFrac(alias conn, T)(ref T field, const double density1, const double density2, const double initSphereFrac) {
+  import dlbc.lb.collision, dlbc.lb.connectivity, dlbc.range, dlbc.lattice;
+  import std.math, std.conv, std.numeric;
+  auto smallSize = gn[0];
+  foreach(immutable i; 0..gn.length) {
+    if ( gn[i] < smallSize ) {
+      smallSize = gn[i];
+    }
+  }
+  immutable r2 = initSphereFrac * initSphereFrac * smallSize * smallSize;
+  double[conn.q] pop0 = 0.0;
+  pop0[0] = 1.0;
+  double[conn.d] dv = 0.0;
+  typeof(pop0) pop1 = density1*eqDist!conn(pop0, dv)[];
+  typeof(pop0) pop2 = density2*eqDist!conn(pop0, dv)[];
+  foreach(immutable p, ref e; field.arr) {
+    double[conn.d] gn, offset;
+    foreach(immutable i; Iota!(0, conn.d) ) {
+      gn[i] = p[i] + M.c[i] * field.n[i] - to!double(field.haloSize);
+      offset[i] = gn[i] - to!double(M.nc[i] * field.n[i] / 2) + 0.5;
     }
     if ( offset.dotProduct(offset) < r2 ) {
       e = pop1;
--- src/dlbc/lb/init.d
+++ src/dlbc/lb/init.d
@@ -13,13 +13,48 @@ import dlbc.fields.init;
 
 import dlbc.logging;
 
+/**
+   Lattice-Boltzmann initial conditions.
+*/
 enum FluidInit {
+  /**
+     No-op, use this only when a routine other than initFluid takes care of things.
+  */
   None,
+  /**
+     Initialize all sites of fluid i with $(D fluidDensity[i]) on all populations.
+  */
   Const,
+  /**
+     Initialize all sites of fluid i with the equilibrium population for density $(D fluidDensity[i]).
+  */
   EqDist,
+  /**
+     Initialize all sites of fluid i with values chosen at random from the interval
+     $(D 0.. 2 fluidDensity[i]) on all populations, such that the average density
+     is $(D fluidDensity[i]) per population.
+  */
   ConstRandom,
+  /**
+     Initialize all sites of fluid i with the equilibrium population for
+     a random density in the interval $(D 0..2 fluidDensity[i]), such that
+     the average density is $(D fluidDensity[i]).
+  */
   EqDistRandom,
+  /**
+     Initialize all sites of fluid i within a radius $(D sphereRadius) from the centre
+     of the system with the equilibrium population for density $(D fluidDensity[i]), and 
+     all other sites  with the equilibrium population for density $(D fluidDensity2[i]).
+  */
   EqDistSphere,
+  /**
+     Initialize all sites of fluid i within a radius $(D sphereRadius * system size)
+     from the centre of the system with the equilibrium population for density
+     $(D fluidDensity[i]), and all other sites  with the equilibrium population
+     for density $(D fluidDensity2[i]). Here, system size is taken to be the shortest
+     axis of the system.
+  */
+  EqDistSphereFrac,
 }
 
 void initFluid(alias conn, T)(ref T field, const size_t i) {
@@ -46,6 +81,9 @@ void initFluid(alias conn, T)(ref T field, const size_t i) {
     break;
   case(FluidInit.EqDistSphere):
     field.initEqDistSphere!conn(fluidDensity[i], fluidDensity2[i], sphereRadius);
+    break;
+  case(FluidInit.EqDistSphereFrac):
+    field.initEqDistSphereFrac!conn(fluidDensity[i], fluidDensity2[i], sphereRadius);
     break;
   }
 }
