1d6672faea38a4e5eba667c8f38a334684ae8f93
Stefan Frijters <sfrijters@gmail.com>
2014-08-08 15:28:14 +0200

    [Bugfix] Dielectrophoretic force.
    Gradient was calculated wrongly, using nb[j] instead of cv[i][j] to
    determine direction.

--- src/dlbc/elec/force.d
+++ src/dlbc/elec/force.d
@@ -16,37 +16,43 @@ void addElecForce(T)(ref T L) if (isLattice!T) {
 
   immutable cv = econn.velocities;
 
-  foreach(immutable p, ef; L.elField ) {
+  foreach(immutable p, ref F; L.forceDistributed ) {
     // Electrostatic force
 
     if ( enableElectrostatic ) {
       foreach(immutable j; Iota!(0, econn.d) ) {
         double[econn.d] electrostatic = elementaryCharge * ( L.elChargeP[p] - L.elChargeN[p] ) * L.elField[p][j];
-        L.forceDistributed[p][j] += electrostatic[j];
+        F[j] += electrostatic[j];
       }
     }
 
-    if ( localDiel && components > 1 && enableDielectrophoretic ) {
-      foreach(immutable i; Iota!(0, econn.q) ) {
+    if ( enableDielectrophoretic && localDiel && components > 1 ) {
+      double[econn.d] grad_E2 = 0.0;
+      foreach(immutable i; 1..cv.length ) {
+        // Calculate neighbour
         econn.vel_t nb;
         foreach(immutable j; Iota!(0, econn.d) ) {
           nb[j] = p[j] - cv[i][j];
         }
+
+        // Calculate E2 at the neighbour
         double nb_E2 = 0.0;
-        double[econn.d] grad_E2 = 0.0;
         foreach(immutable j; Iota!(0, econn.d) ) {
-          nb_E2 += L.elField[p][j] * L.elField[p][j];
+          nb_E2 += L.elField[nb][j] * L.elField[nb][j];
         }
+
+        // Calculate the gradient
         nb_E2 *= 0.5;
         foreach(immutable j; Iota!(0, econn.d) ) {
-          grad_E2[j] += nb_E2 * nb[j];
-        }
-        // Add forces
-        foreach(immutable j; Iota!(0, econn.d) ) {
-          L.forceDistributed[p][j] += 0.5 * ( L.getLocalDiel(p) - averageDiel ) * grad_E2[j];
-            // writeLogRI("force = %s",-0.5 * ( L.getLocalDiel!(T.dimensions)(p) - averageDiel ) * grad_E2[j]);
+          grad_E2[j] += nb_E2 * cv[i][j];
         }
       }
+
+      // Add forces
+      foreach(immutable j; Iota!(0, econn.d) ) {
+        F[j] += 0.5 * ( L.getLocalDiel(p) - averageDiel ) * grad_E2[j];
+      }
+      //writeLogRI("forceDistributed%s = %s", p, L.forceDistributed[p]) ;
     }
   }
 }
