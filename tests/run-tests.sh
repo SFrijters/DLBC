9ed6470a35acba80578e24f6ef243aefbee572e4
Stefan Frijters <sfrijters@gmail.com>
2014-09-05 11:09:34 +0200

    Add test numbering.

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-
+NT=`find . -name reference-data | wc -l`
+CT=0
 for d in `find . -name reference-data`; do
+  CT=$((CT+1))
   TD=`echo $d | sed 's/reference-data//'`
-  echo "=== Testing $TD ==="
+  echo "=== Testing $CT/$NT: $TD ==="
   cd $TD
   make
   cd -
