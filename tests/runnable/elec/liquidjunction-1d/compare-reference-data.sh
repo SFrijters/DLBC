c5419223e2aad1a83e3fdc8a94ec5f3a0b20ac5e
Stefan Frijters <sfrijters@gmail.com>
2015-01-30 20:12:34 +0100

    Convert liquidjunction-1d test.

--- tests/runnable/elec/liquidjunction-1d/compare-reference-data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-for data in elPot elChargeP elChargeN elField elDiel ; do
-
-  h5diff output/${data}-liquidjunction-1d-*-t00001000.h5 reference-data/${data}-liquidjunction-1d-*-t00001000.h5 /OutArray
-
-done
