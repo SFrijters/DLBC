daa744cf13605ba4ba10dbe6b36b610b0d3bc7fa
Stefan Frijters <sfrijters@gmail.com>
2015-02-20 13:24:07 +0100

    Move Travis runnable tests to its own shell script.

---
+++ tests/travis-ci/runnable-tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    DC=dmd
+else
+    DC=$1
+fi
+
+if [ "${DC}" == "dmd" ]; then
+    ./tests/runnable/process-tests.py --dub-compiler ${DC} --only-first
+else
+    echo "Runnable tests are performed only with dmd."
+fi
+
