fbce155211575a13c8850eeaee7631af94aad69a
Stefan Frijters <sfrijters@gmail.com>
2015-03-24 16:45:50 +0100

    Update references for random init tests.

--- tests/runnable/tests/lb/init-2d/plot-colour.py
+++ tests/runnable/tests/lb/init-2d/plot-colour.py
@@ -4,10 +4,12 @@
 from mplhelper import *
 #### END HEADER
 
-g = sorted(glob.glob("reference-data/colour-red-blue-*h5"))
+relpath = "output"
+
+g = sorted(glob.glob(relpath + "/colour-red-blue-*h5"))
 for fn in g:
     print fn
-    dsname = fn.replace("reference-data/colour-red-blue-","").replace("-t00000000.h5","")
+    dsname = fn.replace(relpath + "/colour-red-blue-","").replace("-t00000000.h5","")
     fig, ax = plt.subplots()
     ax.set_xticks([-0.5, 31.5, 63.5])
     ax.set_xticklabels(["0", "32", "64"])
