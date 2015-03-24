3dd8469960753c002c3d70e80bd03cced4365bd1
Stefan Frijters <sfrijters@gmail.com>
2015-03-24 16:11:50 +0100

    Add colour plot script for init condition tests.

---
+++ tests/runnable/tests/lb/init-2d/plot-colour.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+#### START HEADER
+from mplhelper import *
+#### END HEADER
+
+g = sorted(glob.glob("reference-data/colour-red-blue-*h5"))
+for fn in g:
+    print fn
+    dsname = fn.replace("reference-data/colour-red-blue-","").replace("-t00000000.h5","")
+    fig, ax = plt.subplots()
+    ax.set_xticks([-0.5, 31.5, 63.5])
+    ax.set_xticklabels(["0", "32", "64"])
+    ax.set_yticks([-0.5, 31.5, 63.5])
+    ax.set_yticklabels(["0", "32", "64"])
+    
+    f = h5py.File(fn,'r')
+    dset = f['OutArray']
+    colour = dset[:,:]
+    density = plt.imshow(colour)
+    grid(True, which='major')
+    
+    density.set_cmap('bwr')
+    plt.colorbar()
+    plt.axvline(0.5,color='k')
+    plt.axhline(31.5,color='k')
+    plt.show()
+    write_to_file(dsname)
+
+exit()
+
