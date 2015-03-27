ad9d31dc63a9372f7686a97c6128f263cc1b2881
Stefan Frijters <sfrijters@gmail.com>
2015-03-27 11:50:41 +0100

    Allow init colour plots to use different sizes.

--- tests/runnable/tests/lb/init-2d/plot-colour.py
+++ tests/runnable/tests/lb/init-2d/plot-colour.py
@@ -11,14 +11,17 @@ for fn in g:
     print fn
     dsname = fn.replace(relpath + "/colour-red-blue-","").replace("-t00000000.h5","")
     fig, ax = plt.subplots()
-    ax.set_xticks([-0.5, 31.5, 63.5])
-    ax.set_xticklabels(["0", "32", "64"])
-    ax.set_yticks([-0.5, 31.5, 63.5])
-    ax.set_yticklabels(["0", "32", "64"])
     
     f = h5py.File(fn,'r')
     dset = f['OutArray']
     colour = dset[:,:]
+
+    ax.set_xticks([-0.5, colour.shape[1]/2 - 0.5, colour.shape[1] - 0.5])
+    ax.set_xticklabels(["0", str(colour.shape[1]/2), str(colour.shape[1])])
+
+    ax.set_yticks([-0.5, colour.shape[0]/2 - 0.5, colour.shape[0] - 0.5])
+    ax.set_yticklabels(["0", str(colour.shape[0]/2), str(colour.shape[0])])
+
     density = plt.imshow(colour)
     grid(True, which='major')
     
