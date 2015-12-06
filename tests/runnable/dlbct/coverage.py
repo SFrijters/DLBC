dca347c42d9571bbdff3175417a586cc9b19fef0
Stefan Frijters <sfrijters@gmail.com>
2015-12-06 17:59:08 +0100

    Perform coverage merging only for own files - not for dependencies

--- tests/runnable/dlbct/coverage.py
+++ tests/runnable/dlbct/coverage.py
@@ -17,7 +17,7 @@ def moveCovLst(dlbcRoot, configuration):
     """ Move a set of coverage .lst files to the coverage path and add a .tmp suffix. """
     import glob
     covpath = constructCoveragePath(dlbcRoot)
-    for f in glob.glob(os.path.normpath(os.path.join(covpath,"*.lst"))):
+    for f in sorted(glob.glob(os.path.normpath(os.path.join(covpath,"src*.lst")))):
         nf = os.path.basename(f).replace(".lst", "-" + configuration + ".lst.tmp")
         shutil.move(f, os.path.join(covpath, nf))
 
@@ -79,23 +79,25 @@ def mergeCovLstsUnittest(options, covpath):
     logNotification("    Merging unittest coverage information ...")
 
     # Rename the .lst files for the first configuration - the others will be merged into this one.
-    for f in glob.glob(os.path.join(covpath, "*" + dlbcConfigurations[0] + "*.lst.tmp")):
+    for f in sorted(glob.glob(os.path.join(covpath, "src*" + dlbcConfigurations[0] + "*.lst.tmp"))):
         nf = f.replace(".lst.tmp", ".lst").replace("-" + dlbcConfigurations[0],"")
         shutil.move(f, os.path.join(covpath, nf))
 
     # Merge the other configurations into the first.
     for c in dlbcConfigurations[1:]:
-        for f1 in glob.glob(os.path.join(covpath, "*.lst")):
+        for f1 in sorted(glob.glob(os.path.join(covpath, "src*.lst"))):
             f2 = f1.replace(".lst", "-" + c + ".lst.tmp")
             mergeCovLst(f1, f2)
             logDebug("Removing coverage file '" + f2 + "' ...")
             os.remove(f2)
 
+    logDebug("    Files after merge: %s" % sorted(glob.glob(os.path.join(covpath, "src*.lst"))))
+
 def mergeCovLsts(options, testRoot, covpath):
     """ Merge coverage information for a runnable test into the existing coverage files. """
     import glob
     logNotification("    Merging runnable coverage information ...")
-    for f1 in glob.glob(os.path.join(covpath, "*.lst")):
+    for f1 in sorted(glob.glob(os.path.join(covpath, "src*.lst"))):
         f2 = os.path.join(testRoot, os.path.basename(f1))
         mergeCovLst(f1, f2)
 
