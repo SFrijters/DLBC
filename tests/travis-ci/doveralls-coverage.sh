ca1cd9d87b72396899a81a39a5937e12530b92f9
Stefan Frijters <sfrijters@gmail.com>
2015-12-06 16:27:41 +0100

    Only send code coverage to Doveralls for one particular compiler version

--- tests/travis-ci/doveralls-coverage.sh
+++ tests/travis-ci/doveralls-coverage.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
 
+function run_tests {
+    if [ -z "$1" ]; then
+        DUMP=0
+    else
+        DUMP=1
+    fi
+    ./tests/runnable/process-tests.py --coverage --dub-compiler ${DC} --only-first
+    cp tests/coverage/*.lst .
+    if [[ "$DUMP" == 1 ]]; then
+	dub run --compiler ${DC} doveralls -- -d > $1
+    else
+	dub run --compiler ${DC} doveralls
+    fi
+    rm -f *.lst
+}
+
 if [ -z "$1" ]; then
     DC=dmd
 else
     DC=$1
 fi
 
-if [ -z "$2" ]; then
-    DUMP=0
-else
-    DUMP=1
-fi
-
 if [ "${DC}" == "dmd" ]; then
-    ./tests/runnable/process-tests.py --coverage --dub-compiler ${DC} --only-first
-    cp tests/coverage/*.lst .
-    if [[ "$DUMP" == 1 ]]; then
-	dub run --compiler ${DC} doveralls -- -d > $2
+    if [ -z "$2" ]; then
+        run_tests $3
     else
-	dub run --compiler ${DC} doveralls
+        dmd --version | grep "$2"
+        VC=$?
+        if [ "$VC" -eq 0 ]; then
+            run_tests $3
+        else
+            echo "Code coverage is performed only with dmd version $2, but found" `dmd --version`
+        fi
     fi
-    rm -f *.lst
 else
     echo "Code coverage is performed only with dmd."
 fi
